7be4a86d97a14834f6bad92d65732c4dda1841ed
mattsparksy <74625247+mattsparksy@users.noreply.github.com>
2021-08-12 15:49:10 +0100

    done, need to do favicon and build it

--- src/components/topsection.jsx
+++ src/components/topsection.jsx
@@ -1,20 +1,34 @@
-import React from "react";
 import { ImGithub } from "react-icons/im";
 import { SiGmail } from "react-icons/si";
-import { FiTwitter } from "react-icons/fi";
-
+import { FiTwitter, FiCpu } from "react-icons/fi";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import React, { useEffect } from 'react';
 
 
 
 
 export function TopSection() {
-  return (
-    <div className="TopSectionContainer"> 
-        <h1 className="Leftlogos"><ImGithub /></h1> 
-        <h1 className="Leftlogos"><SiGmail /></h1> 
+  
+  useEffect(() => {
+    Aos.init({ duration: 2000 });    
+  }, []);
+  
+  return ( 
 
-        
-    </div> 
+    
+
+    <div>
+      <div className="TopSectionContainer"> 
+          <a data-aos="fade-right" href="https://github.com/mattsparksy" className="Leftlogos"><ImGithub /></a> 
+          <a data-aos="fade-left" href="mailto:mattswyt124@gmail.com" className="Leftlogos"><SiGmail /></a> 
+      </div> 
+      <div className="TopSectionRight">
+          <a data-aos="fade-right" href="https://twitter.com/mattsparksy" className="RightLogos"><FiTwitter /></a> 
+          <a data-aos="fade-left" href="https://mattsparksy.github.io/html-css-prac/work.html" className="RightLogos"><FiCpu /></a>
+      </div>
+    </div>
+
     
   );
 }
